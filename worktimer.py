7bf30f3d3f140975cb0fd6a42798d9939806216c
Sergey <myrik260138@tut.by>
2019-06-17 18:27:24 +0300

    add argparser

--- worktimer.py
+++ worktimer.py
@@ -1,14 +1,12 @@
 #!/bin/python3
-
-import sys
+import argparse
 from datetime import date, datetime, timedelta
-import psutil
 
 import arrow
 
 import django_init
-from helpers.notifier import Notify
 from helpers.lock_detect import is_locked
+from helpers.notifier import Notify
 from worktimer.models import Tick, Wifi
 
 _ = django_init.application
@@ -52,12 +50,12 @@ def main():
                     Notify.show('WORK', 'Welcome home')
                     worktime = Tick.work_time(today)
                     Notify.show(
-                        'Добро пожаловать', 
-                        f'\nЗа сегодня: {worktime}', 
+                        'Добро пожаловать',
+                        f'\nЗа сегодня: {worktime}',
                         'kmousetool_off'
                     )
             Tick.create(
-                wifi=wifi, 
+                wifi=wifi,
                 state=last.State.REST if locked else None
             )
         else:
@@ -73,73 +71,73 @@ def main():
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        main()
+    parser = argparse.ArgumentParser(description='Working time tracker')
+    parser.add_argument('--show', '--today', action='store_true', dest='show', help='Show today status')
+    parser.add_argument('--mouth', action='store_true', help='Show mouth statistic')
+    parser.add_argument('--set', choices=['work', 'home'], help='Set current wifi as...')
+    parser.add_argument('--track', metavar='DATE', help='Track this date as working day')
+    args = parser.parse_args()
 
-    if len(sys.argv) == 2:
-        if sys.argv[1] in ['show', 'today']:
-            tw = Tick.today()
-            worktime = Tick.work_time(tw)
-            tw_last = tw.last()
-            if tw_last and tw_last.wifi.state == Wifi.State.WORK:
-                Notify.show('Рабочий день в процессе', f'Потрачено: {worktime}')
+    if args.show:
+        tw = Tick.today()
+        worktime = Tick.work_time(tw)
+        tw_last = tw.last()
+        if tw_last and tw_last.wifi.state == Wifi.State.WORK:
+            Notify.show('Рабочий день в процессе', f'Потрачено: {worktime}')
+        else:
+            Notify.show('Отдых', f'Потрачено: {worktime}')
+    elif args.mouth:
+        from itertools import groupby
+
+        d = Tick.days(date.today() - timedelta(days=30), date.today())
+
+        weeks = [
+            [(i[0], Tick.work_time(i[1])) for i in v]
+            for k, v in groupby(d, lambda x: x[0].isocalendar()[1])
+        ]
+
+        week_reports = list()
+        for week in weeks:
+            weektime = timedelta(seconds=sum([i[1].seconds for i in week]))
+            if not weektime:
+                continue
+
+            week_report = [f'{d} {t or "-"}' for d, t in week]
+
+            week_need = WORKDAY * min(len(week), 5)
+            if weektime > week_need:
+                week_report += [f'Overtime: {weektime - week_need}']
             else:
-                Notify.show('Отдых', f'Потрачено: {worktime}')
-        elif sys.argv[1] == 'mouth':
-            from itertools import groupby
+                week_report += [f'Overtime: -{week_need - weektime}']
 
-            d = Tick.days(date.today() - timedelta(days=30), date.today())
+            week_reports.append('\n'.join(week_report))
+        print('\n'.join(week_reports))
+    elif args.set:
+        wifi = Tick.today().last().wifi
+        state = args.set
+        if wifi.id == 1:
+            print(f'Wifi disconnected!')
+        elif state == 'work':
+            wifi.state = Wifi.State.WORK
+            wifi.save()
+            print(f'{state}: {wifi.essid}')
+        elif state == 'home':
+            wifi.state = Wifi.State.HOME
+            wifi.save()
+            print(f'{state}: {wifi.essid}')
+    elif args.track:
+        tm = arrow.get(args.track).datetime.replace(tzinfo=None)
+        tm = tm.replace(hour=10)
 
-            weeks = [
-                [(i[0], Tick.work_time(i[1])) for i in v]
-                for k, v in groupby(d, lambda x: x[0].isocalendar()[1])
-            ]
+        w = Wifi.objects.filter(state=Wifi.State.WORK).first()
 
-            week_reports = list()
-            for week in weeks:
-                weektime = timedelta(seconds=sum([i[1].seconds for i in week]))
-                if not weektime:
-                    continue
+        Tick.day(tm.date()).delete()
 
-                week_report = [f'{d} {t or "-"}' for d, t in week]
-
-                week_need = WORKDAY * min(len(week), 5)
-                if weektime > week_need:
-                    week_report += [f'Overtime: {weektime - week_need}']
-                else:
-                    week_report += [f'Overtime: -{week_need - weektime}']
-
-                week_reports.append('\n'.join(week_report))
-            print('\n'.join(week_reports))
-    elif len(sys.argv) == 3:
-        if sys.argv[1] == 'set':
-            wifi = Tick.today().last().wifi
-            state = sys.argv[2].lower()
-            if wifi.id == 1:
-                print(f'Wifi disconnected!')
-            elif state == 'work':
-                wifi.state = Wifi.State.WORK
-                wifi.save()
-                print(f'{state}: {wifi.essid}')
-            elif state == 'home':
-                wifi.state = Wifi.State.HOME
-                wifi.save()
-                print(f'{state}: {wifi.essid}')
-            else:
-                print(f'Unknown status: {state}')
-        elif sys.argv[1] == '+':
-            tm = arrow.get(sys.argv[2].lower()).datetime.replace(tzinfo=None)
-            tm = tm.replace(hour=10)
-
-            w = Wifi.objects.filter(state=Wifi.State.WORK).first()
-
-            Tick.day(tm.date()).delete()
-
-            t = Tick.objects.create(
-                wifi=w,
-                start=tm,
-                end=tm + WORKDAY
-            )
-            print(f"Create {t}")
+        t = Tick.objects.create(
+            wifi=w,
+            start=tm,
+            end=tm + WORKDAY
+        )
+        print(f"Tracked {t}")
     else:
-        print(f'Unknown args: {sys.argv[1:]}')
+        main()
