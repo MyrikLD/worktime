b7e7b7d506b8670be25bdd52180969ad3dea9cff
Myrik <myrik260138@tut.by>
2019-11-04 12:58:44 +0300

    refactor lock detector

--- helpers/lock_detect.py
+++ helpers/lock_detect.py
@@ -1,27 +1,31 @@
 import psutil
 
+_pids_data = {'lock_pid': None, 'last_pids': set()}
 
-def is_locked(pids_data={'lock_pid': None, 'last_pids': set()}):
+
+def is_locked(lock_name='i3lock') -> bool:
     pids = set(psutil.pids())
-    if pids_data.get('lock_pid'):
-        if pids_data['lock_pid'] in pids:
+
+    if _pids_data['lock_pid']:
+        if _pids_data['lock_pid'] in pids:
             return True
         else:
-            pids_data['lock_pid'] = None
-            pids_data['last_pids'] = pids
+            _pids_data['lock_pid'] = None
+            _pids_data['last_pids'] = pids
             return False
 
-    for pid in pids - pids_data['last_pids']:
+    new_pids = pids - _pids_data['last_pids']
+
+    for pid in new_pids:
         try:
-            path = f'/proc/{pid}/cmdline'
-            with open(path, 'rb') as f:
-                data = f.read().replace(b'\x00', b' ').decode()
-            if 'i3lock' in data:
-                pids_data['lock_pid'] = pid
-                pids_data['last_pids'] = pids
+            with open(f'/proc/{pid}/cmdline', 'rb') as f:
+                cmdline = f.read().replace(b'\x00', b' ').decode()
+            if lock_name in cmdline:
+                _pids_data['lock_pid'] = pid
+                _pids_data['last_pids'] = pids
                 return True
-        except IOError: # proc has already terminated
+        except IOError:  # proc has already terminated
             continue
 
-    pids_data['last_pids'] = pids
+    _pids_data['last_pids'] = pids
     return False
